8bef530d227d964cae5ea8bd3d7d861298a56068
hankei6km <gh-hankei6km@outlook.jp>
2022-03-28 14:39:29 +0900

    Edit link label to licenses.zip

--- scripts/license-banner.sh
+++ scripts/license-banner.sh
@@ -6,6 +6,6 @@ cat << EOF
  * $(jq < package.json -r .name)
  * @copyright (c) 2022 hankei6km
  * @license MIT
- * Open Source Licenses: see licenses.zip in ${1}
+ * Open Source Licenses: ${1}
  */
 EOF
